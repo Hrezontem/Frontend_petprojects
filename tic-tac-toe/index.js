6d1009eaf2f5a93947648713995a93d9f3898026
Hrezontem <nikitatarapata@gmail.com>
2024-07-21 15:31:55 +0300

    Пока что не работает игра

---
+++ tic-tac-toe/index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gameItem = document.querySelectorAll('.game__item');
+    const resetBtn = document.querySelector('.reset');
+    let turn = false;
+    let gameMap = [];
+
+    for(let i = 0; i < 3; i++){
+        gameMap[i] = [];
+        for(let j = 0; j < 3; j++){
+            gameMap[i][j] = gameItem[j];
+        }
+    }
+
+    gameItem.forEach((item) => {
+        let flag = false;
+         item.addEventListener('click', function clickTTT(){
+            item.innerHTML = (turn == false && flag === false) ? 'X': 'O'
+            turn = !turn;
+ 
+            if(flag === false){
+                for(let i = 0; i < gameMap.length; i++){
+                    for(let j = 0; j < gameMap[i].length; j++){
+                            console.log('WIN')
+                    }
+                }
+            }
+            flag = true
+
+        })
+        resetBtn.addEventListener('click', () => {
+            turn = false;
+            flag = false;
+            item.innerHTML = '';
+        })
+    })
+});
