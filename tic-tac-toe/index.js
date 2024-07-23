3412e48ea7038ba7281051f66d46ef491437c02d
Hrezontem <nikitatarapata@gmail.com>
2024-07-23 17:16:01 +0300

    modified:   tic-tac-toe/index.js 	modified:   tic-tac-toe/style.css

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -2,18 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const gameItem = document.querySelectorAll('.game__item');
     const resetBtn = document.querySelector('.reset');
     let turn = false;
-    let gameMap = [];
-
-    for(let i = 0; i < 3; i++){
-        gameMap[i] = [];
-        for(let j = 0; j < 3; j++){
-            gameMap[i][j] = gameItem[j];
-        }
-    }
+    let gameMap = [
+        [],
+        [],
+        []
+    ];
+    console.log(gameItem)
 
     gameItem.forEach((item) => {
         let flag = false;
-         item.addEventListener('click', function clickTTT(){
+        // for(let i = 0; i < gameMap.length; i++){
+        //     for(let j = 0; j < 3; j++){
+        //         gameMap[i][j] = item;
+        //         console.log(gameMap)
+        //     }
+        // }
+        item.addEventListener('click', function clickTTT(){
             if (flag === false){
                 item.innerHTML = (turn == false) ? 'X': 'O'
                 turn = !turn;
@@ -22,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
             if(flag === false){
                 for(let i = 0; i < gameMap.length; i++){
                     for(let j = 0; j < gameMap[i].length; j++){
-                            console.log('WIN')
+                            console.log(gameMap[i][j])
                     }
                 }
             }
@@ -35,4 +39,5 @@ document.addEventListener("DOMContentLoaded", () => {
             item.innerHTML = '';
         })
     })
+
 });
