277336d618928dfbeef490f100c05fe5b99db429
Hrezontem <nikitatarapata@gmail.com>
2024-08-15 19:34:02 +0300

    modified:   javascript projects/tic-tac-toe/index.js 	new file:   websites/aura/img/auction-sticker.png 	new file:   websites/aura/img/clock.svg 	new file:   websites/aura/img/user-avatar.png 	modified:   websites/aura/index.html 	modified:   websites/aura/style.css

--- javascript projects/tic-tac-toe/index.js
+++ javascript projects/tic-tac-toe/index.js
@@ -1,43 +1,84 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    const gameItem = document.querySelectorAll('.game__item');
+    const gameItem = Array.from(document.querySelectorAll('.game__item'));
     const resetBtn = document.querySelector('.reset');
+    const text = document.querySelector('.text')
     let turn = false;
-    let gameMap = [
+    let endGame = false;
+    let indexGameItem = 0;
+    let gameMapDiagonal = []; 
+    let gameMapDiagonalReverse = []; 
+    let gameMapVertical;
+    let gameMapHorizontal = [
         [],
         [],
         []
     ];
-    console.log(gameItem)
-
+    for(let i = 0; i < gameMapHorizontal.length; i++){
+        for(let j = 0; j < 3; j++){
+            gameMapHorizontal[i].push(gameItem[indexGameItem]);
+            indexGameItem++;
+        }
+    }
+    console.log(gameMapHorizontal)
+    for (let row = 0; row < gameMapHorizontal.length; row++) {
+        gameMapDiagonal.push(gameMapHorizontal[row][row]);
+        gameMapDiagonalReverse.push(gameMapHorizontal[row][gameMapHorizontal.length - row - 1]);
+    }
+    console.log(gameMapDiagonalReverse)
+    gameMapVertical = gameMapHorizontal.map((val, index) => gameMapHorizontal.map(row => row[index]).reverse())
     gameItem.forEach((item) => {
         let flag = false;
-        // for(let i = 0; i < gameMap.length; i++){
-        //     for(let j = 0; j < 3; j++){
-        //         gameMap[i][j] = item;
-        //         console.log(gameMap)
-        //     }
-        // }
         item.addEventListener('click', function clickTTT(){
-            if (flag === false){
+            if (flag === false && endGame === false){
                 item.innerHTML = (turn == false) ? 'X': 'O'
                 turn = !turn;
-            }
- 
-            if(flag === false){
-                for(let i = 0; i < gameMap.length; i++){
-                    for(let j = 0; j < gameMap[i].length; j++){
-                            console.log(gameMap[i][j])
+                for (var i = 0; i < 2; i++){
+                    if ((gameMapHorizontal[i].every(v => v.innerHTML === 'X')) === true || (gameMapHorizontal[i].every(v => v.innerHTML === 'O')) === true) {
+                        console.log('WIN')
+                        console.log(turn)
+                        endGame = true
+                        break
+                    }
+                    else if ((gameMapVertical[i].every(v => v.innerHTML === 'X')) === true || (gameMapVertical[i].every(v => v.innerHTML === 'O')) === true) {
+                        console.log('WIN')
+                        console.log(turn)
+                        endGame = true
+                        break
+                    }
+                    else if ((gameMapDiagonal.every(v => v.innerHTML === 'X')) === true || (gameMapDiagonal.every(v => v.innerHTML === 'O')) === true) {
+                        console.log('WIN')
+                        console.log(turn)
+                        endGame = true
+                        break
+                    }
+                    else if ((gameMapDiagonalReverse.every(v => v.innerHTML === 'X')) === true || (gameMapDiagonalReverse.every(v => v.innerHTML === 'O')) === true) {
+                        console.log('WIN')
+                        console.log(turn)
+                        endGame = true
+                        break
                     }
                 }
+                flag = true
             }
-            flag = true
+            if(endGame === true){
+                if(turn === true) {
+                    text.innerHTML = 'Выиграл X'
+                }
+                if(turn === false) {
+                    text.innerHTML = 'Выиграл O'
+                }
+            }
 
         })
         resetBtn.addEventListener('click', () => {
             turn = false;
             flag = false;
+            endGame = false
+            text.innerHTML = ''
             item.innerHTML = '';
         })
     })
-
+    
 });
